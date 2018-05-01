49272b2e000e95ca69f14bad0de97bfd326e127d
Artem Demo <artemdemo@gmail.com>
2018-05-01 21:21:20 +0300

    added animation

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # JS modules in content driven website
 
-[conditioner](https://github.com/rikschennink/conditioner)
-
-https://www.smashingmagazine.com/2018/03/lazy-loading-with-conditioner-js/
+This is example of how to lazy load js. Inspired by article: 
+[Lazy Loading JavaScript Modules With ConditionerJS](https://www.smashingmagazine.com/2018/03/lazy-loading-with-conditioner-js/)
 
 ## Commands
 
 * `npm run start` - start server, will be available on http://localhost:8080
 * `npm run build` - build js files
+
+## Packages
+
+* [conditioner](https://github.com/rikschennink/conditioner)
+* [anime.js](https://github.com/juliangarnier/anime) 
--- source/modules/priceCard.js
+++ source/modules/priceCard.js
@@ -1,5 +1,47 @@
+import anime from 'animejs';
+
+/**
+ * Price card
+ * @param element {HTMLElement}
+ */
 const priceCard = (element) => {
-    console.log(element);
+    const headerEl = element.querySelector('.card-header');
+    const titleEl = headerEl.querySelector('h4');
+
+    const options = {
+        easing: 'easeInOutQuad',
+        duration: 150,
+    };
+
+    element.addEventListener('mouseenter', () => {
+        anime(Object.assign({}, options, {
+            targets: element,
+            scale: 1.15,
+        }));
+        anime(Object.assign({}, options, {
+            targets: headerEl,
+            backgroundColor: '#FF7043',
+        }));
+        anime(Object.assign({}, options, {
+            targets: titleEl,
+            color: '#ffffff',
+        }));
+    });
+
+    element.addEventListener('mouseleave', () => {
+        anime(Object.assign({}, options, {
+            targets: element,
+            scale: 1,
+        }));
+        anime(Object.assign({}, options, {
+            targets: headerEl,
+            backgroundColor: '#f7f7f7',
+        }));
+        anime(Object.assign({}, options, {
+            targets: titleEl,
+            color: '#212529',
+        }));
+    });
 };
 
 export default priceCard;
